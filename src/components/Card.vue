57f92f5c9841167d2ff99fa4245da434978fa9ca
facundo <facundovigo@gmail.com>
2019-09-04 14:21:44 -0300

    Core - Initial UI architecture for ViendasYa app.

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card" >
+      <img class="card-img-top" src="" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">John Doe</h4>
+        <p class="card-text">Some example text.</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  
+     computed: {
+       theimage: function () {
+        
+        return '@/assets/Bache1.jpg'
+       }
+     
+        },
+    props:['imagen']
+}
+        
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheNavBAr.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav id="barra-principal" class="navbar fixed-top">
+            <h2 class="text-white" id="ViendasYa">ViendasYa</h2>
+                <form class="form-inline" action="/action_page.php">
+                <input class="form-control mr-sm-2" type="text" placeholder="Buscar">
+                <button class="btn btn-success" type="submit">Buscar</button>
+            </form>
+        </nav>
+
+      
+         
+    </div>    
+</template>
+
+<script>
+
+
+export default {
+  name: 'TheNavBar',
+  components: {
+      
+  }
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
