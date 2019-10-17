cdc09888760b0b3a2a24fe94a37e73ca88b0f16b
facundo <facundovigo@gmail.com>
2019-10-17 19:27:16 -0300

    Fix - Fix cdn

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free';
+//import '@fortawesome/fontawesome-free';
 import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+//import '@fortawesome/fontawesome-free/js/all.js'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faSpinner)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
 Vue.use(VueRouter)
