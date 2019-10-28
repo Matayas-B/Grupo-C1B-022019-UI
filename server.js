2205ee6619c882ce0f6457e99bbe43ebdef48278
OttoOctavius <octaviopompilio@gmail.com>
2019-10-28 15:40:32 -0300

    Code:CORS blocking the communication should be mitigated using these changes. However is not gone yet.

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 8080;
 
 app = express();
 
+
 app.use(serveStatic(path.join(__dirname, '/dist/')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get(/.*/, function (req, res) {
     res.sendfile(__dirname, '/dist/index.html');
 })
--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-const server = 'http://localhost:8080';
-//const server = 'https://viendasya-c1b-backend.herokuapp.com';
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,authorization';  
+//axios.defaults.headers.common['Access-Control-Allow-Methods'] = '*';
+
+//const server = 'http://localhost:8080';
+//export VUE_APP_SERVER="https://viendasya-c1b-backend.herokuapp.com"
+
+const server = process.env.VUE_APP_LOCALSERVER;
 
 const API = {
     get: path => axios.get(`${server}${path}`).then(response => response.data),
