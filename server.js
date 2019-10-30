8ac2e4eebbafef95c147529864078947675ee08d
OttoOctavius <octaviopompilio@gmail.com>
2019-10-30 15:02:40 -0300

    Code:CORS support

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
+const cors = require('cors');
+
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const port = process.env.PORT || 8080;
 
 app = express();
-
+app.use(cors());
 
 app.use(serveStatic(path.join(__dirname, '/dist/')));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.get(/.*/, function (req, res) {
     res.sendfile(__dirname, '/dist/index.html');
