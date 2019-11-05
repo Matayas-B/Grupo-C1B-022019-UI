4ebae9ca8b064d143a445ce3ddf7283697d962a3
OttoOctavius <octaviopompilio@gmail.com>
2019-11-05 16:23:37 -0300

    Fix:Added support to application/json body

--- src/service/api.js
+++ src/service/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const server = process.env.VUE_APP_LOCALSERVER;
 
+axios.defaults.headers.common['Content-Type'] = 'application/json; charset=utf-8';
+
 const API = {
     get: path => axios.get(`${server}${path}`).then(response => response.data),
     put: (path, body) => axios.put(`${server}${path}`, body).then(response => response.data),
