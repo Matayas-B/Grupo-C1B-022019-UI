dfa4af43a8d158e1c99c7da48a403ccdcd2dbc84
facundo <facundovigo@gmail.com>
2019-12-10 00:16:11 -0300

    Se agrega paginacion al historial

--- src/components/HistoryAndPunctuation.vue
+++ src/components/HistoryAndPunctuation.vue
@@ -24,18 +24,18 @@
                     </div>
                     <h2 class="labelColor text-center " > {{$t('history')}}  </h2>
                 </div>
-                <div class="card-body" v-for="p in purchases" :key="p.purchaseId">
+                <div class="card-body" v-for="p in getMenus()" :key="p.purchaseId">
                     <CardHistory :purchase="p" :menu="p.purchasedMenu" />
                 </div>
                 <div class="card-footer" >
 
                     <div class="flex-sm-column">
-<!--                        <ul class="pagination" >-->
-<!--                            <li class="page-item"><a class="page-link"  v-on:click="previus">Previous</a></li>-->
-<!--                            <li class="page-item"  v-for="(k, index) in purchases" :key="index"><a class="page-link" value="0" v-on:click="setPage(index)">{{index}}</a></li>-->
-<!--                            <li class="page-item"><a class="page-link" v-on:click="nextt">Next</a></li>-->
-<!--                            <li class="page-item"><a class="page-link" v-on:click="back">back</a></li>-->
-<!--                        </ul>-->
+                        <ul class="pagination" >
+                            <li class="page-item"><a class="page-link"  v-on:click="previus">Previous</a></li>
+                            <li class="page-item"  v-for="(k, index) in purchases" :key="index"><a class="page-link" value="0" v-on:click="setPage(index)">{{index}}</a></li>
+                            <li class="page-item"><a class="page-link" v-on:click="nextt">Next</a></li>
+                            <li class="page-item"><a class="page-link" v-on:click="back">back</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -46,6 +46,7 @@
 <script>
     import API from "../service/api";
     import CardHistory from "./CardHistory";
+    import chunk from "lodash/chunk" ;
 
     export default {
         name: "HistoryAndPunctuation",
@@ -57,6 +58,8 @@
         },
         data(){
             return{
+                loading: false,
+                page: 0,
                 puntuationtoSend: 0,
                 purchases: []
             }
@@ -66,9 +69,9 @@
                 API.get("/customer/purchase?customerId="+ this.$store.state.user.id)
                     .then(res =>this.callBack(res))
             },
-            callBack(res){
+            callBack(r){
                 //Ordenated from most recent to older
-                this.purchases = res.reverse();
+                this.purchases = chunk(r,2);
             },
 
             puntuate(_serviceId,_menuId){
@@ -82,6 +85,21 @@
                 API.post("/customer/score",message)
                     .then(()=>this.back())
                     //.catch(()=>)
+            },
+            setPage(r){
+                return this.page=r
+            },
+            getMenus(){
+                return this.purchases[this.page]
+            },
+            previus(){
+                if (this.page !== 0) this.page = this.page -1
+            },
+            nextt(){
+                if (this.page !== this.purchases.length -1 ) this.page ++
+            },
+            back(){
+                this.$router.push('/prueba')
             }
         }
 
